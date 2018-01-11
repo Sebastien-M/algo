c8ae8828b7c6610d83d9991ae9e4896c23852845
sebastienM <seb.mandaba@gmail.com>
2018-01-11 12:30:41 +0100

    max done

---
+++ max.py
@@ -0,0 +1,14 @@
+import math
+
+numbers = [1, 19, 28, 3, 37, 8, 371, 3, math.pi, 3 * 3 * 3 * 3 * 4, 1002, 1 ^ 10, math.exp(2), math.log(2)]
+
+
+def max(array):
+    highest = [array[0]]
+    for value in array:
+        if highest[0] < value:
+            highest[0] = value
+    return highest
+
+
+print max(numbers)
